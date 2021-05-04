3993cbc3d293ccc584f6122a8f40d54f18e5918a
Tomasz Budzyn <t.budzyn@tkhtechnology.com>
2021-05-04 21:55:33 +0200

    Refactor

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,8 +1,5 @@
 import { INIT_CODE_HASH } from '../src/constants'
 
-//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
-//import { keccak256 } from '@ethersproject/solidity'
-
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
 //const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
